a8a564c2d4b244cbac75bd25bcb2f8316dfdf49c
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2020-02-25 12:25:39 +0100

    Add test covering LambdaMetaFactory.

--- src/test/java/com/yworks/yguard/obf/BootstrapMethodsTest.java
+++ src/test/java/com/yworks/yguard/obf/BootstrapMethodsTest.java
@@ -28,6 +28,75 @@ public class BootstrapMethodsTest extends AbstractObfuscationTest {
   public TestName name = new TestName();
 
   @Test
+  public void testLambdaMetaFactory() throws Exception {
+    // LambdaMetaFactory bootstrap methods are used only in Java 8 and newer
+    final String testTypeName = "com.yworks.yguard.obf.LambdaMetaFactoryTest";
+    assertTrue("Invalid Java version", 8 <= getMajorVersion());
+
+    final String testMethodName = "void run(java.io.PrintStream)";
+
+    // look for java source code that will be compiled with StringConcatFactory
+    // bootstrap methods
+    final String fileName = "LambdaMetaFactoryTest.txt";
+    final URL source = getClass().getResource(fileName);
+    assertNotNull("Could not resolve " + fileName + '.', source);
+
+    // compile the java source code
+    final Compiler compiler = newCompiler();
+
+    final ArrayList sources = new ArrayList();
+    sources.add(compiler.newUrlSource(testTypeName, source));
+
+    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    compiler.compile(sources, baos);
+
+
+    // store resulting bytecode in temporary files and ...
+    final File inTmp = File.createTempFile(name.getMethodName() + "_in_", ".jar");
+    final File outTmp = File.createTempFile(name.getMethodName() + "_out_", ".jar");
+
+    try {
+      write(baos.toByteArray(), inTmp);
+
+      // ... run obfuscator
+      final StringWriter log = new StringWriter();
+      final GuardDB db = new GuardDB(new File[]{inTmp});
+      db.setDigests(new String[0]);
+      db.remapTo(new File[] {outTmp}, null, new PrintWriter(log), false);
+      db.close();
+
+
+      // finally check if the obfuscated class(es) still work(s) as intended
+      //   determine obfuscated names
+      final Mapper mapper = Mapper.newInstance(log.toString());
+
+      final String mtn = mapper.getTypeName(testTypeName);
+      assertNotNull("Could not find mapping for class " + testTypeName, mtn);
+
+      final String mmn = mapper.getMethodName(testTypeName, testMethodName);
+      assertNotNull("Could not find mapping for method " + testTypeName + '#' + testMethodName, mmn);
+
+      //   load obfuscated class and run test method
+      final ByteArrayOutputStream output = new ByteArrayOutputStream();
+      final ClassLoader cl = URLClassLoader.newInstance(new URL[]{outTmp.toURI().toURL()});
+      final Class obfType = Class.forName(mtn, true, cl);
+      final Method run = obfType.getMethod(mmn, PrintStream.class);
+      run.invoke(null, new PrintStream(output));
+
+      //   check test method output
+      assertEquals(
+              "Wrong test output",
+              String.format("true%n", System.lineSeparator()),
+              new String(output.toByteArray(), "UTF-8"));
+    } finally {
+
+      // clean up and remove temporary files
+      inTmp.delete();
+      outTmp.delete();
+    }
+  }
+
+  @Test
   public void testStringConcatFactory() throws Exception {
     // StringConcatFactory bootstrap methods are used only in Java 11 and newer
     assertTrue("Invalid Java version", 11 <= getMajorVersion());
