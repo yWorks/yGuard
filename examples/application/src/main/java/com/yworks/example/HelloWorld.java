b1bc474f86043021d72f8311d48244cf441eadfb
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2020-02-26 12:07:19 +0100

    Update application example to show a obfuscated method.

--- examples/application/src/main/java/com/yworks/example/HelloWorld.java
+++ examples/application/src/main/java/com/yworks/example/HelloWorld.java
@@ -1,8 +1,11 @@
 package com.yworks.example;
 
 public class HelloWorld {
+    private static String companyName() {
+      return "yWorks";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(String.format("Hello %s", companyName()));
     }
 }
-
