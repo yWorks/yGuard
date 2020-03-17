fdb513141e348fe8e70330d09a4c11c14a75a38b
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2020-03-17 14:47:31 +0100

    Add test case for LambdaMetaFactory.

--- src/test/java/com/yworks/yshrink/java13/InvokeDynamicTest.java
+++ src/test/java/com/yworks/yshrink/java13/InvokeDynamicTest.java
@@ -26,7 +26,7 @@ public class InvokeDynamicTest {
 
   @Test
   public void StringConcatFactoryTest() {
-    // LambdaMetaFactory bootstrap methods are used only in Java 8 and newer
+    // StringConcatFactory bootstrap methods are used only in Java 8 and newer
     final String testTypeName = "com.yworks.yshrink.java13.StringConcatFactoryTest";
 
     final String testMethodName = "void run(java.io.PrintStream)";
@@ -67,6 +67,59 @@ public class InvokeDynamicTest {
       assertEquals(
               "Wrong test output",
               String.format("Hello from concat factory%n", System.lineSeparator()),
+              new String(output.toByteArray()));
+
+      // clean up and remove temporary files
+      inTmp.delete();
+      outTmp.delete();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void LambdaMetaFactoryTest() {
+    // LambdaMetaFactory bootstrap methods are used only in Java 8 and newer
+    final String testTypeName = "com.yworks.yshrink.java13.LambdaMetaFactoryTest";
+
+    final String testMethodName = "void run(java.io.PrintStream)";
+
+    // look for java source code that will be compiled with StringConcatFactory
+    // bootstrap methods
+    final String fileName = "LambdaMetaFactoryTest.txt";
+    final URL source = getClass().getResource(fileName);
+    assertNotNull("Could not resolve " + fileName + '.', source);
+
+    // compile the java source code
+    final com.yworks.util.Compiler compiler = com.yworks.util.Compiler.newCompiler();
+
+    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    compiler.compile(List.of(compiler.newUrlSource(testTypeName, source)), baos);
+
+    try {
+      // store resulting bytecode in temporary files and ...
+      File inTmp = File.createTempFile(name.getMethodName() + "_in_", ".jar");
+      File outTmp = File.createTempFile(name.getMethodName() + "_out_", ".jar");
+      Files.write(inTmp.toPath(), baos.toByteArray());
+
+      // Run shrinker
+      YShrink yShrink = new YShrink(false, "SHA-1,MD5");
+      InOutPair inOutPair = new InOutPair();
+      inOutPair.setIn(inTmp);
+      inOutPair.setOut(outTmp);
+      yShrink.doShrinkPairs(List.of(inOutPair), new AllMainMethodsFilter(), null);
+
+      //   load shrinked class and run test method
+      final ByteArrayOutputStream output = new ByteArrayOutputStream();
+      final ClassLoader cl = URLClassLoader.newInstance(new URL[]{outTmp.toURI().toURL()});
+      final Class shrinkedType = Class.forName("com.yworks.yshrink.java13.LambdaMetaFactoryTest", true, cl);
+      final Method run =  shrinkedType.getMethod("run", PrintStream.class);
+      run.invoke(null, new PrintStream(output));
+
+      //   check test method output
+      assertEquals(
+              "Wrong test output",
+              String.format("Hello from lambda invoke%n", System.lineSeparator()),
               new String(output.toByteArray()));
 
       // clean up and remove temporary files
