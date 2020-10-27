fd81291078873655e5b2962b400786672c33cebb
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2020-10-27 14:57:03 +0100

    Evidently, Networks can connect more than one edge at a time for two endpoints.

--- src/main/java/com/yworks/util/graph/Network.java
+++ src/main/java/com/yworks/util/graph/Network.java
@@ -8,19 +8,21 @@ import java.util.Set;
 public class Network<N, E> {
   Map<N, Set<N>> out = new HashMap<>();
   Map<N, Set<N>> in = new HashMap<>();
-  Map<EndpointPair<N>, E> edges = new HashMap<>();
+  Map<EndpointPair<N>, Set<E>> edges = new HashMap<>();
 
   public void addEdge(N source, N target, E edge) {
     addNode(source, target);
     out.get(source).add(target);
     in.get(target).add(source);
-    edges.put(newPair(source, target), edge);
+    EndpointPair endpointPair = newPair(source, target);
+    addEdge(endpointPair);
+    edges.get(endpointPair).add(edge);
   }
 
   public Set<E> inEdges( N node) {
     Set<E> inEdges = new HashSet<>();
     for (N in: in.get(node)) {
-      inEdges.add(getEdge(in, node));
+      inEdges.addAll(getEdges(in, node));
     }
     return inEdges;
   }
@@ -28,7 +30,7 @@ public class Network<N, E> {
   public Set<E> outEdges( N node) {
     Set<E> outEdges = new HashSet<>();
     for (N out: out.get(node)) {
-      outEdges.add(getEdge(node, out));
+      outEdges.addAll(getEdges(node, out));
     }
     return outEdges;
   }
@@ -37,7 +39,7 @@ public class Network<N, E> {
     Set<E> connectingEdges = new HashSet<>();
     for (N out: out.get(source)) {
       if (out.equals(target)) {
-        connectingEdges.add(getEdge(source, target));
+        connectingEdges.addAll(getEdges(source, target));
       }
     }
     return connectingEdges;
@@ -50,6 +52,12 @@ public class Network<N, E> {
     }
   }
 
+  public void addEdge(EndpointPair<N> endpointPair) {
+    if (!edges.containsKey(endpointPair)) {
+      edges.put(endpointPair, new HashSet<E>());
+    };
+  }
+
   public Set<N> nodes() {
     Set<N> nodes = new HashSet<>(out.keySet());
     nodes.addAll(in.keySet());
@@ -57,15 +65,15 @@ public class Network<N, E> {
   }
 
   public EndpointPair<N> incidentNodes( E edge) {
-    for (Map.Entry<EndpointPair<N>, E> entry : edges.entrySet()) {
-      if (entry.getValue().equals(edge)) {
+    for (Map.Entry<EndpointPair<N>, Set<E>> entry : edges.entrySet()) {
+      if (entry.getValue().contains(edge)) {
         return entry.getKey();
       }
     }
     return null;
   }
 
-  private E getEdge( final N source, final N target ) {
+  private Set<E> getEdges( final N source, final N target ) {
     return edges.get(newPair(source, target));
   }
 
