5279395dc1ca06b1642185d07806349eaca98814
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2026-03-04 18:22:01 +1100

    Remove yShrink

--- src/main/java/com/yworks/yshrink/util/MultiReleaseException.java
+++
@@ -1,8 +0,0 @@
-package com.yworks.yshrink.util;
-
-public class MultiReleaseException extends RuntimeException {
-  @Override
-  public String getMessage() {
-    return "Multi-release archives containing classes in META-INF are incompatible with yGuard.";
-  }
-}
